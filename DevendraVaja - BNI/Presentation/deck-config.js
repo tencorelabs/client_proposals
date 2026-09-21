8238e8ce9e6a8818bf1be31073a839c05876efb5
Devendra Vaja <tencorelabs@gmail.com>
2026-09-22 00:34:59 +0530

    feat(bni): add authentic Chartboost logo to slide 2 pedigree grid

--- DevendraVaja - BNI/Presentation/deck-config.js
+++ DevendraVaja - BNI/Presentation/deck-config.js
@@ -127,7 +127,7 @@ const DEFAULT_SLIDES = [
       { name: "EMC", logo: "Pics/logos/emc.png" },
       { name: "Rakuten (Japan)", logo: "Pics/logos/rakuten.png" },
       { name: "Konami Gaming (Tokyo)", logo: "Pics/logos/konami.png" },
-      { name: "Chartboost", logo: "Pics/logos/chartboost.svg" },
+      { name: "Chartboost", logo: "Pics/logos/chartboost.png" },
       { name: "Vungle", logo: "Pics/logos/vungle.svg" },
       { name: "Fundtech", logo: "Pics/logos/fundtech.svg" }
     ]
